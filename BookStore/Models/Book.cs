3cc264382dc692e3d80fd90b9ce2570dba1ff8ee
tsteele1432 <tsteele842@gmail.com>
2022-03-09 23:29:03 -0700

    bobert

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -15,13 +15,16 @@ namespace BookStore.Models
         [Key]
         [Required]
         public int BookId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
         public string Publisher { get; set; }
         public string Isbn { get; set; }
         public string Classification { get; set; }
         public string Category { get; set; }
         public int PageCount { get; set; }
+        [Required]
         public double Price { get; set; }
     }
 }
